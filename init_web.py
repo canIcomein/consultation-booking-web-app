b84c714e0b41398f8c21d1ce5eac94704e10f8d7
Broke <bgolubev94@gmail.com>
2024-06-09 21:47:32 +0300

    Initial commit

---
+++ init_web.py
@@ -0,0 +1,9 @@
+cd E:\pyCharm\Psycho_bot\web_app
+
+git init
+git add .
+git commit -m "Initial commit"
+
+git remote add origin https://github.com/canicomein/consultation-booking-web-app.git
+git branch -M main
+git push -u origin main
---
+++ script.js
@@ -0,0 +1,9 @@
+function bookConsultation() {
+    alert('Booking a consultation...');
+    // Add logic to book consultation
+}
+
+function viewBookings() {
+    alert('Viewing bookings...');
+    // Add logic to view bookings
+}
