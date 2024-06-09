410d26a97333b76579edfa25c4792f9f69868204
Broke <bgolubev94@gmail.com>
2024-06-10 00:23:20 +0300

    Initial commit

--- init_web.py
+++ init_web.py
@@ -1,9 +1,28 @@
-cd E:\pyCharm\Psycho_bot\web_app
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-git init
-git add .
-git commit -m "Initial commit"
+# Обработчик команды /start
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+    update.message.reply_text(f"Hello, {user.first_name}!")
 
-git remote add origin https://github.com/canicomein/consultation-booking-web-app.git
-git branch -M main
-git push -u origin main
+# Обработчик текстовых сообщений
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+# Функция для создания и запуска бота
+def main():
+    # Инициализация бота
+    updater = Updater("7484546517:AAFqufJ4vGvaQcNbTBuE-d6GHuRo8rmqH_I", use_context=True)
+    dp = updater.dispatcher
+
+    # Добавление обработчиков команд
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Запуск бота
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
